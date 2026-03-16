ef624c1d6fd23f605f4d64a094102d24f6eebbc9
localadmin <localadmin@ASUS-HOME>
2026-03-16 18:37:46 +0200

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+
+        char[,] chars = {
+            { 'a', 'b', 'O' },
+            { 'd', 't', 'f' },
+            { 'E', 'h', 'i' }
+        };
+
+
+        Matrix matrix = new Matrix(chars);
+
+
+        Console.WriteLine($"Кількість голосних у масиві: {matrix.Vowels}");
+        Console.WriteLine($"Головна діагональ [0]: {matrix[0]}");
+        Console.WriteLine($"Побічна діагональ [1]: {matrix[1]}");
+
+
+        Console.ReadLine();
+    }
+}
